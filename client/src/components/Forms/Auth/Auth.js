88cb6dcf34e5fe5a31aa5dac324fae5a072f3f8b
hassamboi <hassamuddin.all@gmail.com>
2022-01-04 05:59:09 +0500

    Added doctor portal to staff dashboard

--- client/src/components/Forms/Auth/Auth.js
+++ client/src/components/Forms/Auth/Auth.js
@@ -57,7 +57,18 @@ export default function Auth({ isPatient, handleSwitch }) {
             {isPatient ? "Patient Login" : "Staff Login"}
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus onChange={e => setEmail(e.target.value)} value={email} />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={e => setEmail(e.target.value)}
+              value={email}
+            />
 
             <TextField
               margin="normal"
@@ -92,7 +103,13 @@ export default function Auth({ isPatient, handleSwitch }) {
                 </Link>
               </Grid>
             </Grid>
-            <Button style={{ backgroundColor: colors.accentBlue, color: "black" }} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} onClick={handleSwitch}>
+            <Button
+              style={{ backgroundColor: colors.accentBlue, color: "black" }}
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleSwitch}
+            >
               Switch to {isPatient ? "Staff Login" : "Patient Login"}
             </Button>
           </Box>
